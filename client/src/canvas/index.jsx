5587429330177ed309e1a3a5238f99c9ea3de043
Montywilliams <3650@holbertonschool.com>
2023-04-04 16:42:39 -0600

    correct spelling on imports

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
-import { Canvas } from '@react/three/fiber'
-import { Enviornment, Center } from '@react/three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Enviornment, Center } from '@react-three/drei'
 
 import Shirt from './Shirt'
 import Backdrop from './Backdrop'
 import CameraRig from './CameraRig'
 
-const Canvas = () => {
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
-  )
+    <Canvas>
+        <ambientLight  intensity={0.5}/>
+        <Enviornment preset="city" />
+        <CameraRig>
+            <Backdrop />
+            <Center>
+                <Shirt />
+            </Center>
+        </CameraRig>
+
+    </Canvas>
+    )
 }
 
-export default Canvas
+export default CanvasModel
