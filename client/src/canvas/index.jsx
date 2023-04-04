d716a1a9f633e2cafaf40f3e6b0a0824effe1e70
Montywilliams <3650@holbertonschool.com>
2023-04-04 16:38:06 -0600

    import components and dependencies

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import { Canvas } from '@react/three/fiber'
+import { Enviornment, Center } from '@react/three/drei'
+
+import Shirt from './Shirt'
+import Backdrop from './Backdrop'
+import CameraRig from './CameraRig'
 
 const Canvas = () => {
   return (
