444206bbb23c0de7cf60073b6d8c2070d3ee7cd5
Montywilliams <3650@holbertonschool.com>
2023-04-04 16:55:20 -0600

    import requirements: utilize useGLTF hook to add 3d obj

--- client/src/canvas/Shirt.jsx
+++ client/src/canvas/Shirt.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { easing } from 'maath'
+import { useSnapshot } from 'valtio'
+import { useFrame } from '@react-three/fiber'
+import { Decal, useGLTF, useTexture } from '@react-three/drei'
 
 function Shirt() {
+    const snap = useSnapshot(state)
+    const { nodes, materials } = useGLTF('/shirt_baked.glb')
   return (
     <div>Shirt</div>
   )
