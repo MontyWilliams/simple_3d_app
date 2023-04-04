5dc7a588d1a270887b022598ec94427362d8506f
Montywilliams <3650@holbertonschool.com>
2023-04-04 10:45:04 -0600

    create logic for dynamic button styling:  create generateStyle fucntion based on type baing either filled or not: will require state to be read

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 function CustomButton({type, title, customStyles, handleClick}) {
+    const generateStyle = (type) => {
+        if(type === 'filled') {
+            return {
+                backgroundColor: '#000',
+                color: '#fff'
+            }
+        }
+    }
   return (
     <button
         className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+        style={generateStyle(type)}
     >
         {title}
     </button>
