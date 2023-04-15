a263f021b0091bac135bce7ed65a5637f9746baa
Montywilliams <3650@holbertonschool.com>
2023-04-15 06:32:59 -0600

    pass file state to Filepicker component gp

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -29,7 +29,10 @@ const Customizer = () => {
           case "colorpicker":
             return <ColorPicker />
           case "filepicker":
-            return <FilePicker />
+            return <FilePicker
+                file={file}
+                setFile={setFile}
+            />
           case "aipicker":
             return <AIPicker />
           default:
