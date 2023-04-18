817766b8eebf5bfd803e8970b37a441307c60e26
Montywilliams <3650@holbertonschool.com>
2023-04-18 07:33:38 -0600

    correct error

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -3,14 +3,17 @@ import CustomButton from './CustomButton'
 
 function FilePicker({ file, setFile, readFile }) {
   return (
-    <div classNanme="filepicker-container">
+    <div className="filepicker-container">
       <div className="flex-1 flex flex-col">
       <input
         id="file-upload"
         type="file"
         accept="image/*"
         onChange={(e) => setFile(e.target.files[0])}
-      />  
+      />
+      <label htmlFor="file-upload" className="filepicker-label">
+        Uplad File
+      </label>
       </div>  
     </div>
   )
