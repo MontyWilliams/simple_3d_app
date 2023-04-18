46cd8e6a29bb877b96c176d5880f4e5a948c4004
Montywilliams <3650@holbertonschool.com>
2023-04-18 07:46:53 -0600

    add custom buttons

--- client/src/components/FilePicker.jsx
+++ client/src/components/FilePicker.jsx
@@ -5,19 +5,33 @@ function FilePicker({ file, setFile, readFile }) {
   return (
     <div className="filepicker-container">
       <div className="flex-1 flex flex-col">
-      <input
-        id="file-upload"
-        type="file"
-        accept="image/*"
-        onChange={(e) => setFile(e.target.files[0])}
-      />
-      <label htmlFor="file-upload" className="filepicker-label">
-        Uplad File
-      </label>
-      <p className="mt-2 text-gray-500 text-xs truncate">
-        {file === '' ? "Pick a file " : file.name}
-      </p>
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          onChange={(e) => setFile(e.target.files[0])}
+        />
+        <label htmlFor="file-upload" className="filepicker-label">
+          Uplad File
+        </label>
+        <p className="mt-2 text-gray-500 text-xs truncate">
+          {file === '' ? "Pick a file " : file.name}
+        </p>
 
+        <div className="mt-4 flex flex-wrap gap-35">
+          <CustomButton
+            type="outline"
+            title="Logo"
+            handleClick={() => readFile('logo')}
+            customStyles="text-xs"
+          />
+          <CustomButton
+            type="filled"
+            title="Full"
+            handleClick={() => readFile('full')}
+            customStyles="text-xs"
+          />
+        </div>
       </div>  
     </div>
   )
