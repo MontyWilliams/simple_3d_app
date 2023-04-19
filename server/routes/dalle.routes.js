bbef1e6ffc4ab67f1c7ad321f40414456e2d7475
Montywilliams <3650@holbertonschool.com>
2023-04-19 10:51:59 -0600

    add route for homepage

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -2,4 +2,11 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import { Configuration, OpenAIApi } from 'openai';
 
-dotenv.config()
+dotenv.config();
+
+const router = express.Router();
+
+router.route('/').get((req, res ) => {
+    res.status(200).json({ message: "Yo Yo Yo Mfer"})
+})
+ export default router
