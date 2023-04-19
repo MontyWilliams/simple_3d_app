9ed125667c45b72b149696206781eca83313095c
Montywilliams <3650@holbertonschool.com>
2023-04-19 10:45:20 -0600

    server should listen on por8080: create get req for home page

--- server/index.js
+++ server/index.js
@@ -8,3 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }))
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: "Yo Yo Yo from Dalle"})
+})
+
+app.listen(8080, () => console.log('Server has started on port 8080'))
