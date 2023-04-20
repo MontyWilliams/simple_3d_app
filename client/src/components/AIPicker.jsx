c57f7886ece813fd9ddb4370f79de8b18bc01028
Montywilliams <3650@holbertonschool.com>
2023-04-19 19:05:43 -0600

    add text area usingstyled classes

--- client/src/components/AIPicker.jsx
+++ client/src/components/AIPicker.jsx
@@ -6,7 +6,13 @@ import CustomButton from './CustomButton.jsx';
 function AIPicker({ prompt, setPrompt, generatingImg, handleSubmit }) {
   return (
     <div className="aipicker-container">
-      
+      <textarea
+        className="aipicker-textarea"
+        placeholder="Ask AI..."
+        rows={5}
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+      />
 
     </div>
   )
